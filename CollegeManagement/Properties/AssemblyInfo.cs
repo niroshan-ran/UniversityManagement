10a01eb8d147526476b180468bc6dbe7f035a78a
nirosshan-ran <inranasinghe2208@gmail.com>
2020-09-23 20:35:28 +0530

    Another Commit by Niroshan

--- CollegeManagement/Properties/AssemblyInfo.cs
+++ CollegeManagement/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CollegeManagement")]
+[assembly: AssemblyTitle("College Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CollegeManagement")]
+[assembly: AssemblyCompany("SLIIT Titans Coperation")]
+[assembly: AssemblyProduct("College Management")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
