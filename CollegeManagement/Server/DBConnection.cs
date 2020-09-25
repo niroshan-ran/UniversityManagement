79cc054221a6f24b6635a969bd3cca76b4f341f2
nirosshan-ran <inranasinghe2208@gmail.com>
2020-09-25 19:17:06 +0530

    Another Commit by Niroshan

--- CollegeManagement/Server/DBConnection.cs
+++ CollegeManagement/Server/DBConnection.cs
@@ -1,7 +1,7 @@
 ﻿using CollegeManagement.Properties;
 namespace CollegeCore
 {
-    public class DBConnection
+    public static class DBConnection
     {
         public static string ConnectionString = @"datasource=localhost;username=root;password='root'";
         public static string connectionStringNew = @Settings.Default.CollegeDBConnectionString;
