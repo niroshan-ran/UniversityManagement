f653fb3e53ecbe098517de5e37b5bcbba7fa66b5
nirosshan-ran <inranasinghe2208@gmail.com>
2020-09-25 10:44:56 +0530

    Logic Updates by Niroshan

--- CollegeManagement/WorkingDaysHours/TimeSlots.Designer.cs
+++ CollegeManagement/WorkingDaysHours/TimeSlots.Designer.cs
@@ -222,6 +222,8 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(477, 150);
             this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // TimeSlots
             // 
